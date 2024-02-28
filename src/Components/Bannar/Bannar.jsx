0a5012bf1e47769a3903c291ebd6f602daaa3a65
MIRNOMAN <you@example.comabdullahalnoman1509@gmail.com>
2024-02-29 03:54:05 +0600

    product card section

--- src/Components/Bannar/Bannar.jsx
+++ src/Components/Bannar/Bannar.jsx
@@ -5,11 +5,11 @@ import bannar3 from "../../../public/images/hero/product3.png"
 const Bannar = () => {
   return (
     <div >
-        <div className="lg:flex lg:h-[400px] lg:mx-[13%] lg:my-14 gap-4">
+        <div className="lg:flex  lg:h-[400px] lg:mx-[13%] lg:my-14 gap-4">
                  
-                 <div className="lg:w-1/2 h-[410px]">
-                     <img className="relative" src={bannar1} alt="bannar image" />
-                     <div className="absolute lg:-mt-40 -mt-44 p-6">
+                 <div className="lg:w-1/2 bg-fixed h-[410px]">
+                     <img className="relative bg-fixed" src={bannar1} alt="bannar image" />
+                     <div className="absolute  bg-fixed lg:-mt-40 -mt-44 p-6">
                            <h1 className="text-4xl font-medium">Wooden Water Bottles </h1>
                            <div className="flex justify-around items-center">
                            <span className=" uppercase mt-2 font-medium">Up to 60% off</span>
@@ -18,10 +18,10 @@ const Bannar = () => {
                      </div>
                  </div>
 
-                 <div className="lg:w-1/2 ">
+                 <div className="lg:w-1/2 bg-fixed ">
                        <div className="lg:h-1/2 mb-3 h-[200px] ">
-                       <img className="relative" src={bannar2} alt="bannar image" />
-                     <div className="absolute lg:-mt-40 -mt-40 p-6">
+                       <img className="relative bg-fixed" src={bannar2} alt="bannar image" />
+                     <div className="absolute bg-fixed lg:-mt-40 -mt-40 p-6">
                            <h1 className="text-3xl font-medium">Wooden Water Bottles </h1>
                            <span className=" uppercase mt-2 font-medium">Up to 60% off</span>
                            <div className="">
@@ -31,9 +31,9 @@ const Bannar = () => {
                      </div>
                        </div>
 
-                       <div className="lg:h-1/2 mt-2 h-[200px]">
-                       <img className="relative" src={bannar3} alt="bannar image" />
-                     <div className="absolute lg:-mt-40 -mt-44 lg:pl-48 pl-44 p-5">
+                       <div className="lg:h-1/2 bg-fixed mt-2 h-[200px]">
+                       <img className="relative bg-fixed" src={bannar3} alt="bannar image" />
+                     <div className="absolute lg:-mt-40 bg-fixed -mt-44 lg:pl-48 pl-44 p-5">
                            <h1 className="text-3xl font-medium">Mauve Leather Bag  </h1>
                            <span className=" uppercase mt-2 font-medium">Up to 60% off</span>
                            <div className="">
--- src/utils/ProductCards.jsx
+++ src/utils/ProductCards.jsx
@@ -1,8 +1,66 @@
+import {IoCart} from "react-icons/io5"
+import {IoMdHeart} from "react-icons/io"
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa"
+import { NavLink } from "react-router-dom"
+
+ export const RanderRatingStars = (rating) =>{
+    const totallStars = 5;
+    const fullStarts = Math.floor(rating);
+    const hasHalfStarts = rating % 1 !== 0;
+    const stars =[];
+    for(let i = 1; i <= totallStars; i++){
+        if(i <= fullStarts){
+            stars.push(<FaStar key={i} color="#ff8a00"></FaStar>);
+        }else if(hasHalfStarts && i === fullStarts + 1){
+            stars.push(<FaStarHalfAlt key="half-star" color="#ff8a00"></FaStarHalfAlt>);
+        }else{
+            stars.push(<FaRegStar key={i} color="#ff8a00"></FaRegStar>);
+        }
+    }
+    return stars;
+ }
 
 
 const ProductCards = ({id, title, description, images,price,discount,rating,featured,category,color}) => {
   return (
-    <div>ProductCards</div>
+    <>
+     <div className="product cart">
+         <div className="images  h-96">
+             {images?.map((cover, index) =>(
+                <img key={index} src={cover?.image} alt={id} className="w-full h-full object-cover"/>
+             ))}
+             <div className="flex justify-between w-full p-5 absolute top-0 left-0">
+              {discount && <button className="discount-btn">{discount}%</button>}
+              {featured && <button className="feature-btn">{featured === true && "Featured"}</button>}
+             </div>
+             <div className="overlay  flex  items-center gap-2 justify-center absolute bottom-0 left-0 right-0 m-5">
+                <button className="quick-view-btn product-btn primary-btn">Quick view</button>
+                <button className="add-to-cart-btn product-btn primary-btn">
+                    <IoCart size={23}></IoCart>
+                </button>
+                <button className="love-btn product-btn primary-btn">
+                    <IoMdHeart size={23}></IoMdHeart>
+                </button>
+             </div>
+         </div>
+         <div className="details flex items-center flex-col bg-white pb-3 pt-6">
+           <NavLink to={`/product-details/${id}`}>
+            <span>{title}</span>
+           </NavLink>
+           <div className="flex items-center gap-2 mt-1 mb-2">{RanderRatingStars(rating)}</div>
+           <div className="flex items-center gap-3">
+              {price?.slice(0,1).map((priceItem, index) =>(
+                <>
+                <p className="line-through" key={index}>${priceItem.value}</p>
+                <p className="text-primary-green" key={index}>
+                    ${(priceItem.value - (priceItem.value * discount) /100).toFixed(2)}
+                    </p>
+                </>
+              ))}
+           </div>
+         </div>
+     </div>
+    </>
   )
 }
 
