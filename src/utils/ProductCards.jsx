8a38748383fa5c78348cfc8903ae6c23d3b7eb7e
MIRNOMAN <you@example.comabdullahalnoman1509@gmail.com>
2024-02-29 22:15:01 +0600

    create open modal  product details

--- src/utils/ProductCards.jsx
+++ src/utils/ProductCards.jsx
@@ -1,67 +1,107 @@
-import {IoCart} from "react-icons/io5"
-import {IoMdHeart} from "react-icons/io"
+import { IoCart } from "react-icons/io5"
+import { IoMdHeart } from "react-icons/io"
 import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa"
 import { NavLink } from "react-router-dom"
+import { useState } from "react"
 
- export const RanderRatingStars = (rating) =>{
+export const RanderRatingStars = (rating) => {
     const totallStars = 5;
     const fullStarts = Math.floor(rating);
     const hasHalfStarts = rating % 1 !== 0;
-    const stars =[];
-    for(let i = 1; i <= totallStars; i++){
-        if(i <= fullStarts){
+    const stars = [];
+    for (let i = 1; i <= totallStars; i++) {
+        if (i <= fullStarts) {
             stars.push(<FaStar key={i} color="#ff8a00"></FaStar>);
-        }else if(hasHalfStarts && i === fullStarts + 1){
+        } else if (hasHalfStarts && i === fullStarts + 1) {
             stars.push(<FaStarHalfAlt key="half-star" color="#ff8a00"></FaStarHalfAlt>);
-        }else{
+        } else {
             stars.push(<FaRegStar key={i} color="#ff8a00"></FaRegStar>);
         }
     }
     return stars;
- }
+}
 
 
-const ProductCards = ({id, title, description, images,price,discount,rating,featured,category,color}) => {
-  return (
-    <>
-     <div className="product cart">
-         <div className="images  h-96">
-             {images?.map((cover, index) =>(
-                <img key={index} src={cover?.image} alt={id} className="w-full h-full object-cover"/>
-             ))}
-             <div className="flex justify-between w-full p-5 absolute top-0 left-0">
-              {discount && <button className="discount-btn">{discount}%</button>}
-              {featured && <button className="feature-btn">{featured === true && "Featured"}</button>}
-             </div>
-             <div className="opacity-0 hover:opacity-100 transition-all duration-300  flex  items-center gap-2 justify-center absolute bottom-0 left-0 right-0 hover:bottom-6  m-5">
-                <button className="quick-view-btn product-btn primary-btn">Quick view</button>
-                <button className="add-to-cart-btn product-btn primary-btn">
-                    <IoCart size={23}></IoCart>
-                </button>
-                <button className="love-btn product-btn primary-btn">
-                    <IoMdHeart size={23}></IoMdHeart>
-                </button>
-             </div>
-         </div>
-         <div className="details flex items-center flex-col bg-white pb-3 pt-6">
-           <NavLink to={`/product-details/${id}`}>
-            <span>{title}</span>
-           </NavLink>
-           <div className="flex items-center gap-2 mt-1 mb-2">{RanderRatingStars(rating)}</div>
-           <div className="flex items-center gap-3">
-              {price?.slice(0,1).map((priceItem, index) =>(
+const ProductCards = ({ id, title, description, images, price, discount, rating, featured, category, color }) => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    }
+
+
+    return (
+
+        <>
+            <div className="product cart">
+                <div className="images  h-96">
+                    {images?.map((cover, index) => (
+                        <img key={index} src={cover?.image} alt={id} className="w-full h-full object-cover" />
+                    ))}
+                    <div className="flex justify-between w-full p-5 absolute top-0 left-0">
+                        {discount && <button className="discount-btn">{discount}%</button>}
+                        {featured && <button className="feature-btn">{featured === true && "Featured"}</button>}
+                    </div>
+                    <div className="opacity-0 hover:opacity-100 transition-all duration-300  flex  items-center gap-2 justify-center absolute bottom-0 left-0 right-0 hover:bottom-2  m-5">
+                        <button onClick={openModal} className="quick-view-btn product-btn primary-btn">Quick view</button>
+                        <button className="add-to-cart-btn product-btn primary-btn">
+                            <IoCart size={23}></IoCart>
+                        </button>
+                        <button className="love-btn product-btn primary-btn">
+                            <IoMdHeart size={23}></IoMdHeart>
+                        </button>
+                    </div>
+                </div>
+                <div className="details flex items-center flex-col bg-white pb-3 pt-6">
+                    <NavLink to={`/product-details/${id}`}>
+                        <span>{title}</span>
+                    </NavLink>
+                    <div className="flex items-center gap-2 mt-1 mb-2">{RanderRatingStars(rating)}</div>
+                    <div className="flex items-center gap-3">
+                        {price?.slice(0, 1).map((priceItem, index) => (
+                            <>
+                                <p className="line-through" key={index}>${priceItem.value}</p>
+                                <p className="text-primary-green" key={index}>
+                                    ${(priceItem.value - (priceItem.value * discount) / 100).toFixed(2)}
+                                </p>
+                            </>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+            {isModalOpen && (
                 <>
-                <p className="line-through" key={index}>${priceItem.value}</p>
-                <p className="text-primary-green" key={index}>
-                    ${(priceItem.value - (priceItem.value * discount) /100).toFixed(2)}
-                    </p>
+                    <div className="overlay-bg" onClick={closeModal}>
+                        <div className="modal-overlay" onClick={closeModal}>
+                            <div className="modal-content flex justify-between" onClick={(e) => e.stopPropagation()}>
+
+                                <div className="w-1/2 h-[500px] overflow-hidden">
+                                    {images.slice(0, 1)?.map((cover, index) => (
+                                        <img key={index} src={cover?.image} alt={id} className="modal-image w-full h-full object-cover" />
+                                    ))}
+                                </div>
+
+                                <div className="modal-details w-1/2 h-[500px] overflow-y-scroll p-9">
+                                <button className="feature-btn bg-indigo-500">SALE {discount}% OFF</button>
+                                <h1 className="text-xl mt-2">{title}</h1>
+                                <div className="flex items-center gap-1 mt-2">
+                                   {RanderRatingStars(rating)}
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </>
-              ))}
-           </div>
-         </div>
-     </div>
-    </>
-  )
+            )}
+
+        </>
+    )
 }
 
 export default ProductCards
