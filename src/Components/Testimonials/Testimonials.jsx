7be229eefccbd06e16716b1adcf08574c2516911
MIRNOMAN <you@example.comabdullahalnoman1509@gmail.com>
2024-03-03 15:41:07 +0600

    testimonial section update

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,13 +1,49 @@
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 
+function SampleNextArrow (props) {
+    const {onClick} = props;
+    return(
+        <div className="absolute bottom-0 right-0 bg-white text-primary rounded-full slider-btn" onClick={onClick}>
+            <button className="next">
+              <MdKeyboardArrowRight size={30}/>
+            </button>
+        </div>
+    )
+
+}
+
+function SamplePrevArrow (props) {
+    const {onClick} = props;
+    return(
+        <div className="absolute bottom-0 right-20 bg-white text-primary rounded-full slider-btn z-10" onClick={onClick}>
+            <button className="next">
+              <MdKeyboardArrowLeft size={30}/>
+            </button>
+        </div>
+    )
+
+}
 const Testimonials = () => {
+    const settings = {
+        dots : false,
+        infinite: true,
+        slidesToShow :1,
+        slidesToScroll :1,
+        nextArrow : <SampleNextArrow/>,
+        prevArrow : <SamplePrevArrow/>,
+        
+    }
   return (
     <>
        <section className="testimonials">
-          <div className="container h-full flex items-center justify-center">
+          <div className="container  flex items-center justify-center">
             <div className="w-1/2 flex relative z-50">
-                <div className="box w-96 h-96 bg-white rounded-full z-50">
-                    <img src="../../../public/images/hero/girl.png" alt="" className="absolute -top-[130px] left-0 z-10 rounded-b-full " />
+                <div className="box w-48 h-48 bg-white rounded-full z-50">
+                    <img src="../../../public/images/hero/girl.png" alt="" className="absolute w-[350px] h-[350px] -bottom-32 left-0 z-10 rounded-b-full " />
                 </div>
                 <div className="bg-[rgba(255,255,255,0.5)] px-5 backdrop-blur-sm p-3 rounded-lg absolute top-36 right-10 z-50">
                     <p className="leading-none">Our Satisfied User</p>
@@ -18,6 +54,22 @@ const Testimonials = () => {
                     </div>
                 </div>
             </div>
+            <div className="lrft w-1/2 mt-20 relative z-50">
+                <h1 className="text-3xl font-semibold">What our clients say abouts us</h1>
+                <p className="mt-4">It is long established fact that a reader will be distracted by the readable content off a page when lookings at its layout .The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters..</p>
+                <Slider {...settings}>
+                <TestimonialsCard 
+                 name="Kenneth Pomer"
+                 post="Undergraduate students"
+                 img="../../../public/images/testimonial/pic5.jpg"
+                />
+                <TestimonialsCard 
+                 name="joe Doe"
+                 post="Undergraduate students"
+                 img="../../../public/images/testimonial/pic6.jpg"
+                />
+                </Slider>
+            </div>
           </div>
         </section> 
     </>
@@ -25,3 +77,23 @@ const Testimonials = () => {
 }
 
 export default Testimonials
+
+
+
+
+
+export const TestimonialsCard = ({img, name, post}) => {
+  return (
+    <>
+        <div className="flex items-center gap-8">
+          <div className="w-20 h-20">
+            <img src={img} alt="" className="h-full w-full object-cover rounded-full" />
+          </div>
+          <div className="details">
+           <p className="font-medium leading-none">{name}</p>
+           <p>{post}</p>
+          </div>
+        </div>
+    </>
+  )
+}
