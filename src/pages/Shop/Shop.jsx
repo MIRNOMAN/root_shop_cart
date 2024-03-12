34caa062d818fcb88249fda7603e15883a6749c2
MIRNOMAN <you@example.comabdullahalnoman1509@gmail.com>
2024-03-12 17:23:30 +0600

    product filter by category

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,22 +1,34 @@
+import { useState } from "react";
 import { productlists } from "../../assets/data/data"
 import ProductCards from "../../utils/ProductCards"
 
 
 const Shop = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredProductLists = selectedCategory
+    ? productlists.filter(item => item.title === selectedCategory)
+    : productlists;
   return (
     <>
     <section className="flex">
     <div className="px-7 ml-12 bg-slate-50 lg:w-1/5 mt-16 ">
      <div className="mt-3">
-      <h1 className="text-center text-lg font-semibold  my-3">Categories</h1>
+      <h1 className="text-center text-lg font-semibold my-3">Categories</h1>
       {productlists?.map((item) => (
-        <button key={item.id} className="border w-full mb-2 px-8 py-2 text-sm hover:bg-[#0d775e] hover:text-white rounded-md text-center">{item.title}</button>
+        <button key={item.id} 
+        onClick={() => handleCategoryClick(item?.title)}
+        className={`border w-full mb-2 px-8 py-2 text-sm hover:bg-[#0d775e] hover:text-white rounded-md text-center ${selectedCategory === item.category ? 'bg-[#0d775e] text-white' : ''}`}>{item.title}</button>
       ))}
     
      </div>
       </div>
         <div className="px-7 lg:w-4/5 mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {productlists.map((item) => (
+        {filteredProductLists?.map((item) => (
                       <ProductCards
                       id={item.id} 
                       key={item.id}
